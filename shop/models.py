3042155cd2600fbd8e73dd786d91dc61aa170a42
officexchangesg <officexchangesg@gmail.com>
2022-06-06 18:14:37 +0800

    mycom

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.urls import reverse
+from django.core.files.storage import FileSystemStorage
+
+media_storage = fs = FileSystemStorage(location='shop/media')
+
 class Category(models.Model):
     name = models.CharField(max_length=200,
                             db_index=True)
@@ -21,7 +26,7 @@ class Product(models.Model):
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
+    image = models.ImageField(storage=media_storage,upload_to='products/%Y/%m/%d',
                               blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
