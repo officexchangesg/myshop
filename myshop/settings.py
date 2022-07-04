8c3a09e27a8c4e412c095c9c3d22869c78fc80e6
officexchangesg <officexchangesg@gmail.com>
2022-07-04 10:02:55 +0800

    final

--- myshop/settings.py
+++ myshop/settings.py
@@ -20,13 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(a)p2fvg4#vdj%_!m3i&q37w22f3essme)r^kxk(v2(hvlo(o5'
+# SECRET_KEY = 'django-insecure-(a)p2fvg4#vdj%_!m3i&q37w22f3essme)r^kxk(v2(hvlo(o5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+ 
+# ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = []
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
